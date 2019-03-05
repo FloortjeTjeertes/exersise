355f620a3b2b28346512ac39108c9eed64f1c857
floriantjeertes <43434642+floriantjeertes@users.noreply.github.com>
2019-03-05 17:53:39 +0100

    Add files via upload
    2 opdrachten af zonder css

---
+++ exersise/rechthoekscript.js
@@ -0,0 +1,34 @@
+//begin Rechthoek
+let lengte = document.getElementById('lengte');
+let breete = document.getElementById('breete');
+let oppervlakte = document.getElementById('oppervlakte');
+let omtrek = document.getElementById('omtrek');
+let berekenKnop = document.getElementById('bereken');
+let rechthoek = document.getElementById('rechthoek');
+let diagonaal =document.getElementById('diagonaal');
+let ctx = rechthoek.getContext("2d");
+
+bereken.addEventListener('click', berkening);
+
+function berkening() {
+  console.log("je hebt op de knop geklickt");
+  if (isNaN(lengte.value) == false && isNaN(breete.value) == false) {
+    console.log("je hebt een nummer in gevoerd");
+    oppervlakte.innerHTML = parseInt(lengte.value) * parseInt(breete.value);
+    omtrek.innerHTML = 2 * parseInt(lengte.value) + 2 * parseInt(breete.value);
+    diagonaal.innerHTML = Math.sqrt(Math.pow(parseInt(lengte.value), 2) + Math.pow(parseInt(breete.value), 2));
+    console.log(diagonaal);
+    //rechthoek tekening test
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 5 * parseInt(breete.value), 5 * parseInt(lengte.value));
+    //einde Rechthoek
+
+
+
+  } else {
+    console.log("niet een cijfer ingevuld");
+  }
+}
+
+
+//einde rechthoek
